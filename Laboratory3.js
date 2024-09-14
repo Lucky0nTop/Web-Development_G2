f812b2e43b6bd3e90351cd816bb93b4b34e6c14f
xMiguelBolano <miguelangelbolanolopez@gmail.com>
2024-09-14 17:22:44 -0500

    Se agregarón los test para confirmar las relaciones entre clases, además se agregarón nuevos métodos

--- Laboratory3.js
+++ Laboratory3.js
@@ -8,13 +8,21 @@ class Agencia {
     calcularVentas(){
         let totalVentas = 0;
         this.tiquetes.forEach(tiquete => {
-            totalVentas += tiquete.getPrecio();
+            if (tiquete.vendido){
+                totalVentas += tiquete.getPrecio();
+            }
         });
-        return totalVentas;
+        return "EL total de ventas es: " + totalVentas;
     }
 
-    agregarTiquetes(){
+    agregarTiquetes(tiquete){
         this.tiquetes.push(tiquete);
+    }
+
+    mostrarTiquetes(){
+        this.tiquetes.forEach(tiquete => {
+            console.log(`Origen: ${tiquete.origen}, Destino: ${tiquete.destino}, Número de silla: ${tiquete.numerodesilla}, Hora: ${tiquete.hora}, Fecha: ${tiquete.fecha}, Precio: ${tiquete.precio}`, "Disponibilidad: " + tiquete.disponilidad());
+        });
     }
 
     agregarBus(bus){
@@ -54,16 +62,6 @@ class Silla {
     }
 }
 
-// const bus = new Bus("ABC-123", 45);
-// // Crear instancias de la clase Silla
-// const silla1 = new Silla(1, "Silla delantera", true);
-// const silla2 = new Silla(2, "Silla trasera", false);
-// // Agregar las sillas al bus
-// bus.agregarSilla(silla1);
-// bus.agregarSilla(silla2);
-// // Mostrar las sillas del bus
-// bus.mostrarSillas();
-
 class Tiquete {
     constructor (origen, destino, numerodesilla, hora, fecha, precio, bus, pasajero){
         this.origen = origen;
@@ -74,6 +72,7 @@ class Tiquete {
         this.precio = precio;
         this.bus = bus;
         this.pasajero = pasajero;
+        this.vendido = false;
     }
 
     setBus(bus){
@@ -87,6 +86,15 @@ class Tiquete {
     getPrecio(){
         return this.precio;
     }
+
+    disponilidad(){
+        if (this.pasajero){
+            this.vendido = true;
+            return "No";
+        } else {
+            return "Sí";
+        }
+    }
 }
 
 class Pasajero {
@@ -97,7 +105,38 @@ class Pasajero {
         this.edad = edad;
     }
 
-    getInformacion(){
-        return `Nombre: ${this.nombre}, Identificación: ${this.identificacion}, Teléfono: ${this.telefono}, Edad: ${this.edad}`;
+    mostrarInformacion(){
+        console.log(`Nombre: ${this.nombre}, Identificación: ${this.identificacion}, Teléfono: ${this.telefono}, Edad: ${this.edad}`);
     }
 }
+
+
+// Test pasajero
+const pasajero1 = new Pasajero("Juan", "123456", "3023234", 25);
+pasajero1.mostrarInformacion();
+const pasajero2 = new Pasajero("Pedro", "123457", "3023235", 30);
+
+// Crear instancias de la clase Silla
+const silla1 = new Silla(1, "Silla delantera", true);
+const silla2 = new Silla(2, "Silla trasera", false);
+
+// Test bus
+const bus1 = new Bus("ABC-123", 45);
+// Agregar las sillas al bus
+bus1.agregarSilla(silla1);
+bus1.agregarSilla(silla2);
+// Mostrar las sillas del bus
+bus1.mostrarSillas();
+
+// Test tiquete
+const tiquete1 = new Tiquete("Maicao", "Cali", 1, "10:00", "2021-06-01", 50000, bus1, pasajero1);
+const tiquete2 = new Tiquete("Maicao", "Cali", 2, "10:00", "2021-06-01", 50000, bus1, );
+
+// Test agencia
+const agencia1 = new Agencia("Copetran");
+agencia1.agregarBus(bus1);
+agencia1.mostrarListaBuses();
+agencia1.agregarTiquetes(tiquete1);
+agencia1.agregarTiquetes(tiquete2);
+agencia1.mostrarTiquetes();
+console.log(agencia1.calcularVentas());
